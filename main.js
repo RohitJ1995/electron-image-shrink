410b57ce936849b8ff68916b5b39f7491c797bcd
Rohit Jayarama <rohitj@getvymo.com>
2020-10-21 01:12:26 +0530

    - use platform vaiables - use mac secific features for close and activate

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const {app, BowserWindow, BrowserWindow} = require('electron');
 
+process.env.NODE_ENV = 'development';
+
+const isDev = process.env.NODE_ENV == 'development' ? true : false;
+const isMac = process.platform === 'darwin' ? true : false;
+
 let mainWindow;
 
 function createMainWindow(){
@@ -7,10 +12,24 @@ function createMainWindow(){
         title: 'ImageShrink',
         height: 500,
         width: 600,
-        icon: `${__dirname}/assets/icons/Icon_256x256.png`
+        icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+        resizable: isDev
     })
     mainWindow.loadURL(`file://${__dirname}/app/index.html`)
 
 }
-
+app.allowRendererProcessReuse = true;
 app.on('ready', createMainWindow)
+app.on('window-all-closed', () => {
+    if (!isMac) {
+        // for mac to exit the app on Cmd +Q
+      app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      // for mac to recreate the new window whwn clicked on the dock icon to open new window
+      createMainWindow()
+    }
+})
